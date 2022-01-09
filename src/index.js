30283c1ae9edc3b3ad9f00d3b625de4baa553e1c
Marten Richter <marten.richter@tu-berlin.de>
2022-01-09 10:41:48 +0100

    Add service worker stuff

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import './index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import App from './app'
+
+// taken from the CRA pwa template by google
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register()
 
 ReactDOM.render(<App />, document.getElementById('root'))
