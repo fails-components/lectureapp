56b20de6b7d4ea4c9bbafafcd4dfa85be4db5e45
Marten Richter <marten.richter@tu-berlin.de>
2022-04-17 12:47:36 +0200

    Initial E2E key exchange

--- src/socketworker.js
+++ src/socketworker.js
@@ -1,6 +1,10 @@
 import { io } from 'socket.io-client'
 // eslint-disable-next-line camelcase
 import jwt_decode from 'jwt-decode'
+import {
+  serialize as BSONserialize,
+  deserialize as BSONdeserialize
+} from 'bson'
 
 class SocketWorker {
   constructor(args) {
@@ -15,23 +19,270 @@ class SocketWorker {
     this.handleBlackboard = this.handleBlackboard.bind(this)
 
     this.periodicStatusCheck = this.periodicStatusCheck.bind(this)
+
+    // key generation
+    // eslint-disable-next-line no-restricted-globals
+    this.cryptKeyPair = crypto.subtle.generateKey(
+      {
+        name: 'RSA-OAEP',
+        modulusLength: 4096,
+        publicExponent: new Uint8Array([1, 0, 1]),
+        hash: 'SHA-256'
+      },
+      true,
+      ['encrypt', 'decrypt']
+    )
+
+    // eslint-disable-next-line no-restricted-globals
+    this.signKeyPair = crypto.subtle.generateKey(
+      {
+        name: 'ECDSA',
+        namedCurve: 'P-384'
+      },
+      true,
+      ['sign', 'verify']
+    )
+
+    this.idents = {}
+
+    this.keyobject = { exptime: Date.now() / 1000 + 10 } // we have 20 seconds for a new key
+
+    const bidarray = new Uint32Array(2)
+    crypto.getRandomValues(bidarray)
+    this.keymasterBidding = Math.abs(
+      ((0xfffff & bidarray[0]) << 32) | bidarray[1]
+    )
     // eslint-disable-next-line no-restricted-globals
     self.setInterval(this.periodicStatusCheck, 1000)
   }
 
-  periodicStatusCheck() {
+  async periodicStatusCheck() {
     const token = this.decodedToken()
+    const now = Date.now() / 1000
     let expired = true
-    if (token && token.exp && token.exp - Date.now() / 1000 > 0) expired = false
+    if (token && token.exp && token.exp - now > 0) expired = false
 
     if (this.tokenexpired !== expired) {
       this.setExpiredToken(expired)
       this.tokenexpired = expired
     }
+
+    const keyobject = this.keyobject
+    if (!keyobject || (keyobject.exptime && keyobject.exptime - now < 0)) {
+      // keys are not present or expired
+      if (
+        this.socket &&
+        (!this.keymasterQueryTime || this.keymasterQueryTime - now < 0)
+      ) {
+        this.socket.emit('keymasterQuery') // tells the handlers, to query the keymaster
+        this.keymasterQueryTime = now + 20 // give them 20 seconds to fix this
+        this.keymaster = false
+      }
+    } else if (
+      this.socket &&
+      this.keymaster &&
+      (!keyobject.exptime || keyobject.exptime - now - 60 < 0)
+    ) {
+      /* console.log(
+        'keymasterGenerate',
+        !keyobject.exptime,
+        keyobject.exptime - now - 60
+      ) */
+      // I am the keymaster and I should regenerate keys
+      if (!this.keymasterGeneratingKeys) {
+        this.keymasterGeneratingKeys = true
+        try {
+          await this.generateKeysAndSend()
+        } catch (error) {
+          console.log('generateKeys failed', error)
+        }
+        this.keymasterGeneratingKeys = false
+      }
+    }
   }
 
   netSendSocket(command, data) {
     if (this.socket) this.socket.emit(command, data)
+  }
+
+  informUpdatedKeys() {
+    console.log(
+      'new keypair',
+      this.keyobject.keynum,
+      'master digest',
+      new Uint16Array(this.keyobject.digest).join(' ')
+    )
+  }
+
+  async sendAndEncrypt(el) {
+    try {
+      let tosend = { ...this.keyobject.sendKOforRec }
+      if (
+        el.purpose === 'notes' ||
+        el.purpose === 'lecture' ||
+        el.purpose === 'screen'
+      )
+        tosend = { ...this.keyobject.sendKO }
+
+      if (tosend.keyRec) {
+        // eslint-disable-next-line no-restricted-globals
+        tosend.keyRec = self.crypto.subtle.encrypt(
+          {
+            name: 'RSA-OAEP'
+          },
+          el.cryptKey,
+          tosend.keyRec
+        )
+      }
+      if (tosend.keyE2E) {
+        // eslint-disable-next-line no-restricted-globals
+        tosend.keyE2E = self.crypto.subtle.encrypt(
+          {
+            name: 'RSA-OAEP'
+          },
+          el.cryptKey,
+          tosend.keyE2E
+        )
+      }
+      tosend.dest = el.id
+      tosend.verifyKey = crypto.subtle.exportKey(
+        'jwk',
+        this.signKeyPair.publicKey
+      )
+      await Promise.all([tosend.keyRec, tosend.keyE2E, tosend.verifyKey])
+
+      if (tosend.keyRec) tosend.keyRec = new Uint8Array(await tosend.keyRec)
+      if (tosend.keyE2E) tosend.keyE2E = new Uint8Array(await tosend.keyE2E)
+      tosend.verifyKey = await tosend.verifyKey
+
+      tosend = BSONserialize(tosend)
+
+      const cryptMess = {
+        message: tosend,
+        signature: await crypto.subtle.sign(
+          {
+            name: 'ECDSA',
+            hash: { name: 'SHA-384' }
+          },
+          this.signKeyPair.privateKey,
+          tosend
+        ),
+        dest: el.id,
+        purpose: el.purpose
+      }
+      this.socket.emit('sendKey', cryptMess)
+    } catch (error) {
+      console.log('error in send and encrypt')
+    }
+  }
+
+  async generateSendKeyObjs() {
+    const exptime = this.keyobject.exptime
+    const keyRec = this.keyobject.keyRec
+    const keyE2E = this.keyobject.keyE2E
+    const keynum = this.keyobject.keynum
+
+    const sendKO = {
+      exptime,
+      keyRec: crypto.subtle.exportKey('raw', keyRec),
+      keyE2E: crypto.subtle.exportKey('raw', keyE2E),
+      keynum
+    }
+    await Promise.all([sendKO.keyRec, sendKO.keyE2E])
+    sendKO.keyRec = await sendKO.keyRec
+    sendKO.keyE2E = await sendKO.keyE2E
+
+    const sendKOforRec = {
+      exptime,
+      keyRec: sendKO.keyRec,
+      id: this.socket.id,
+      keynum
+    }
+
+    this.keyobject.sendKO = sendKO
+    this.keyobject.sendKOforRec = sendKOforRec
+  }
+
+  async generateKeysAndSend() {
+    try {
+      if (!this.keymaster && !this.socket) return // safety sanity
+      let keyE2E = crypto.subtle.generateKey(
+        {
+          name: 'AES-GCM',
+          length: 256
+        },
+        true,
+        ['encrypt', 'decrypt']
+      )
+
+      let keyRec = crypto.subtle.generateKey(
+        {
+          name: 'AES-GCM',
+          length: 256
+        },
+        true,
+        ['encrypt', 'decrypt']
+      )
+
+      const exptime = Date.now() / 1000 + 60 * 5
+
+      let keynum = new Promise((resolve, reject) => {
+        this.socket.emit('getKeyNum', (data) => {
+          if (data.keynum && Number.isInteger(data.keynum)) {
+            resolve(data.keynum)
+          } else reject(new Error('getKeyNum failed'))
+        })
+      })
+
+      const pubkey = crypto.subtle.exportKey('raw', this.signKeyPair.publicKey)
+
+      await Promise.all([keyE2E, keyRec, keynum, pubkey])
+
+      keyE2E = await keyE2E
+      keyRec = await keyRec
+      keynum = await keynum
+
+      this.keyobject = {
+        exptime,
+        keyRec,
+        keyE2E,
+        digest: crypto.subtle.digest('SHA-256', await pubkey),
+        id: this.socket.id,
+        keynum
+      }
+
+      await this.generateSendKeyObjs()
+
+      // ok, now we iterate over all identies and send out the stuff
+      const nowborder = Date.now() - 60 * 5 * 1000
+      const allprom = Object.keys(this.idents)
+        .map((id) => ({ id, ...this.idents[id] }))
+        .filter((el) => el.id !== this.socket.id)
+        .filter((el) => nowborder - Number(el.lastaccess) < 0)
+        .map(this.sendAndEncrypt.bind(this))
+
+      this.keyobject.digest = await this.keyobject.digest
+
+      await Promise.all(allprom)
+      this.informUpdatedKeys()
+    } catch (error) {
+      console.log('generateKeysAndSend failed', error)
+    }
+  }
+
+  async connectCrypto() {
+    // setup crypto stuff
+    this.cryptKeyPair = await this.cryptKeyPair
+    this.signKeyPair = await this.signKeyPair
+    // ok we have the keys, now send the public part to the handler
+    const keyinfo = {
+      cryptKey: await crypto.subtle.exportKey(
+        'jwk',
+        this.cryptKeyPair.publicKey
+      ),
+      signKey: await crypto.subtle.exportKey('jwk', this.signKeyPair.publicKey)
+    }
+    this.socket.emit('keyInfo', keyinfo)
   }
 
   connectNotepad() {
@@ -122,6 +373,200 @@ class SocketWorker {
       // console.log('newauthtoken' /* , this.myauthtoken */)
       console.log('authtoken renewed')
       this.scheduleReauthor() // request renewal
+    })
+
+    const toIden = async (iden, id) => ({
+      signKey: await crypto.subtle.importKey(
+        'jwk',
+        iden.signKey,
+        {
+          name: 'ECDSA',
+          namedCurve: 'P-384'
+        },
+        true,
+        ['verify']
+      ),
+      cryptKey: await crypto.subtle.importKey(
+        'jwk',
+        iden.cryptKey,
+        {
+          name: 'RSA-OAEP',
+          modulusLength: 4096,
+          publicExponent: new Uint8Array([1, 0, 1]),
+          hash: 'SHA-256'
+        },
+        true,
+        ['encrypt']
+      ),
+      purpose: iden.purpose,
+      lastaccess: Number(iden.lastaccess),
+      displayname: iden.displayname,
+      myself: id === this.socket.id
+    })
+
+    this.socket.removeAllListeners('identUpdate')
+    this.socket.on('identUpdate', async (data) => {
+      if (data.id && data.identity) {
+        const iden = data.identity
+        // console.log('identUpdate', iden, iden.signKey, iden.cryptKey, data.id)
+        try {
+          const el = await toIden(iden, data.id)
+          this.idents[data.id] = el
+
+          if (this.keymaster && data.id !== this.socket.id) {
+            await this.generateSendKeyObjs()
+            await this.sendAndEncrypt({ ...el, id: data.id })
+          }
+        } catch (error) {
+          console.log('error identUpdate', error)
+        }
+      }
+    })
+
+    this.socket.removeAllListeners('identValidity')
+    this.socket.on('identValidity', (data) => {
+      console.log('identValidity', data)
+      if (data.id && data.lastaccess) {
+        this.idents[data.id].lastaccess = Number(data.lastaccess)
+      }
+    })
+
+    this.socket.removeAllListeners('identDelete')
+    this.socket.on('identDelete', (data) => {
+      if (data.id) {
+        delete this.idents[data.id].lastaccess
+      }
+    })
+
+    this.socket.removeAllListeners('identList')
+    this.socket.on('identList', async (data) => {
+      const now = Date.now()
+      for (const id in data) {
+        /*  console.log(
+          'identList',
+          this.socket.id,
+          id,
+          data[id],
+          data[id].signKey,
+          data[id].cryptKey,
+          now - 60 * 5 * 1000 - Number(data[id].lastaccess),
+          now - 60 * 5 * 1000,
+          Number(data[id].lastaccess),
+          now - 60 * 5 * 1000 - Number(data[id].lastaccess) < 0
+        ) */
+        if (
+          data[id].signKey &&
+          data[id].cryptKey &&
+          now - 60 * 5 * 1000 - Number(data[id].lastaccess) < 0
+        ) {
+          const newid = toIden(data[id], id)
+          this.idents[id] = await newid
+          // console.log('identList added', id)
+        } else {
+          delete this.idents[id]
+        }
+      }
+    })
+
+    this.socket.removeAllListeners('keymasterQuery')
+    this.socket.on('keymasterQuery', async () => {
+      // ok folks
+      console.log('keymasterQuery')
+      this.socket.emit('keymasterQueryResponse', {
+        keyMaster: !!this.keymaster, // tell them, that I feel, that I am the keyMaster
+        bidding: this.keymasterBidding
+      })
+      this.keymaster = false // if someone queries the keyMaster property is reset
+    })
+
+    this.socket.removeAllListeners('keymasterQueryResponse')
+    this.socket.on('keymasterQueryResponse', async (data) => {
+      console.log('keymasterQueryResponse', data)
+      if (data.keymaster) {
+        console.log('New keymaster!')
+        this.keymaster = true
+        delete this.keyobject.exptime // trigger key regeneration asap
+      } else {
+        console.log('Not keymaster!')
+        this.keymaster = false
+      }
+    })
+
+    this.socket.removeAllListeners('receiveKey')
+    this.socket.on('receiveKey', async (data) => {
+      // console.log('receiveKey', this.keymaster)
+      if (!data.message) return
+      try {
+        const message = BSONdeserialize(data.message)
+        if (message.id === this.socket.id) {
+          console.log('Receiving own key, bad things happening')
+          return
+        }
+
+        if (message.keyRec) {
+          // console.log('message inspect', message)
+          message.keyRec = await crypto.subtle.decrypt(
+            {
+              name: 'RSA-OAEP'
+            },
+            this.cryptKeyPair.privateKey,
+            message.keyRec.buffer
+          )
+        }
+        if (message.keyE2E) {
+          message.keyE2E = await crypto.subtle.decrypt(
+            {
+              name: 'RSA-OAEP'
+            },
+            this.cryptKeyPair.privateKey,
+            message.keyE2E.buffer
+          )
+        }
+        // console.log('data stuff', data)
+        let verikey = null
+
+        if (data.id && this.idents[data.id]) {
+          verikey = this.idents[data.id].signKey
+        }
+        if (!verikey) {
+          // console.log('key info', message, message.id, this.idents)
+          if (message.verifyKey && this.decodedToken().purpose !== 'lecture') {
+            verikey = await crypto.subtle.importKey(
+              'jwk',
+              message.verifyKey,
+              {
+                name: 'ECDSA',
+                namedCurve: 'P-384'
+              },
+              true,
+              ['verify']
+            )
+          } else throw new Error('no key for verifing')
+        }
+        // now we have to verify
+        const verified = await crypto.subtle.verify(
+          {
+            name: 'ECDSA',
+            hash: { name: 'SHA-384' }
+          },
+          verikey,
+          data.signature,
+          data.message
+        )
+        if (!verified) throw new Error('verification of keymaster failed')
+        // console.log('destination check', data.dest, message.dest)
+        if (this.socket.id !== message.dest)
+          throw new Error('destination forged')
+
+        this.keyobject = message
+        this.keyobject.digest = await crypto.subtle.digest(
+          'SHA-256',
+          await crypto.subtle.exportKey('raw', verikey)
+        )
+        this.informUpdatedKeys()
+      } catch (error) {
+        console.log('receiveKey error', error)
+      }
     })
 
     // TODO
@@ -230,6 +675,7 @@ class SocketWorker {
       setTimeout(() => {
         this.socket.emit('sendboards', {})
       }, 100)
+      this.connectCrypto()
       this.scheduleReauthor()
     })
   }
@@ -243,6 +689,7 @@ class SocketWorker {
           notepadsocket.emit('sendboards', {})
         }, 500) */
       // this.updateSizes() // inform sizes
+      this.connectCrypto()
       this.scheduleReauthor()
     })
   }
@@ -256,6 +703,7 @@ class SocketWorker {
           notepadsocket.emit('sendboards', {})
         }, 500) */
       // this.updateSizes() // inform sizes
+      this.connectCrypto()
       this.scheduleReauthor()
     })
   }
@@ -266,6 +714,9 @@ class SocketWorker {
       this.socket.disconnect()
       this.socket = null
     }
+    this.idents = {}
+    this.keyobject = { exptime: Date.now() / 1000 + 10 } // we have 10 seconds for a new key
+    this.keymaster = false
   }
 
   servererrorhandler(code, message, type) {
