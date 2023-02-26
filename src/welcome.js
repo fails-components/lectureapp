404e2efc984d85415f0353d00ad2bfe62e3936fa
Marten Richter <marten.richter@tu-berlin.de>
2023-02-26 17:47:20 +0100

    Add missing license headers

--- src/welcome.js
+++ src/welcome.js
@@ -1,3 +1,21 @@
+/*
+    Fails Components (Fancy Automated Internet Lecture System - Components)
+    Copyright (C)  2015-2017 (original FAILS), 
+                   2021- (FAILS Components)  Marten Richter <marten.richter@freenet.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 import React, { Component } from 'react'
 import failsLogoLong from './logo/logo1.svg'
 import failsLogoLongExp from './logo/logo1exp.svg'
